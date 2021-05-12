40e57c681ca7a2d152312197a62ace5c47522ff8
mlanca-c <mlanca-c@student.42lisboa.com>
2021-05-12 22:56:20 +0100

    Added comentary

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,21 +6,21 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 18:07:37 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/05/12 17:30:41 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/05/12 22:56:08 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
- * This function converts a ASCII string to an integer.
- * The atoi() function converts the initial portion of the string pointed to
- * by str to int representation.
- *
- * @param	const char	*str	- string to be converted to int.
- *
- * @return
- * 		- The ft_atoi() function returns the int representation of the string.
+** This function converts a ASCII string to an integer.
+** The atoi() function converts the initial portion of the string pointed to
+** by str to int representation.
+**
+** @param	const char	*str	- string to be converted to int.
+**
+** @return
+** 		- The ft_atoi() function returns the int representation of the string.
 */
 int	ft_atoi(const char *str)
 {
--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,19 +6,19 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 13:04:24 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/05/12 22:44:22 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/05/12 22:55:33 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
- * The ft_lstiter() function iterates the list 'lst' and applies the function
- * 'f' to the content of each element.
- *
- * @param	t_list	*lst		- address of a pointer to an element.
- * @param	void (*f)(void *)	- address of the function used to iterate on the
- * 								list.
+** The ft_lstiter() function iterates the list 'lst' and applies the function
+** 'f' to the content of each element.
+**
+** @param	t_list	*lst		- address of a pointer to an element.
+** @param	void (*f)(void *)	- address of the function used to iterate on the
+** 								list.
 */
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
