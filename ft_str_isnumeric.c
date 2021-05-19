459318f7ee9e990bd9ab1b89e4d24708e896ebe3
mlanca-c <mlanca-c@student.42lisboa.com>
2021-05-19 13:20:32 +0100

    Create ft_str_isnumeric.c status     - OK commentary - OK testing    - KO

--- ft_str_isnumeric.c
+++ ft_str_isnumeric.c
@@ -1,18 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_isdigit.c                                   :+:      :+:    :+:   */
+/*   ft_str_isnumeric.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 19:59:47 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/05/18 20:03:13 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/05/19 09:17:17 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_str_isdigit(char *str)
+/*
+** This function checks if the string 'str' is a numeric string.
+** The ft_str_isnumeric() function iterates a string and checks if all the
+** characters are digits.
+**
+** @param	char	*str	- string to check if it contains only digits.
+**
+** @return
+** 		- The ft_str_isnumeric() function returns 1, in case the string 'str' is
+** 		a numeric string; or returns 0 in case it isn't.
+*/
+int	ft_str_isnumeric(char *str)
 {
 	int	i;
 
