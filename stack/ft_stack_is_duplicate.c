f0a9ba31595b0258e1b4d073c19b1f0688d937b6
MLSTT <marialsottomayor@gmail.com>
2021-05-19 18:13:18 +0100

    Update ft_stack_is_duplicate.c commentary - OK testing    - KO status     - OK

---
+++ stack/ft_stack_is_duplicate.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stack_is_duplicate.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/19 16:30:46 by mlanca-c          #+#    #+#             */
+/*   Updated: 2021/05/19 16:39:50 by mlanca-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** This function checks if 'data' already exists in the stack.
+** The ft_stack_is_duplicate() function iterates the stack and checks if 'data'
+** already exists.
+**
+** @param	t_stack	*stack	- stack to iterate from.
+** @param	int		data	- data to check if value exists.
+**
+** @return
+** 		- The ft_stack_is_duplicate() function returns 1 if the value of 'data'
+** 		already exists in the 'stack'; or it returns 0 if it doesn't exist.
+*/
+int	ft_stack_is_duplicate(t_stack *stack, int data)
+{
+	if (!stack)
+		return (1);
+	while (stack)
+	{
+		if (stack->data == data)
+			return (1);
+		stack = stack->next;
+	}
+	return (0);
+}
