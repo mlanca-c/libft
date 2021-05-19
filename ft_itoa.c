aa7f72fa15bb4d2076391a0be58d6939c70e5e8c
mlanca-c <mlanca-c@student.42lisboa.com>
2021-05-19 13:15:44 +0100

    Create ft_itoa.c status     - OK commentary - OK testing    - KO

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,41 +6,11 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 17:33:36 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/05/12 22:03:22 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/05/19 10:12:39 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/*
-** This function gets the number of digits of an integer.
-** The get_len() function returns the number of digits of an integer - the
-** length that the string - that represents the integer - will need to have.
-**
-** @param	int	n	- integer to convert in ft_itoa().
-**
-** @return
-** 		- The number of digits of 'n'.
-*/
-int	get_len(int n)
-{
-	int		l;
-
-	l = 0;
-	if (n == 0)
-		return (1);
-	if (n < 0)
-	{
-		n *= -1;
-		l++;
-	}
-	while (n > 0)
-	{
-		n /= 10;
-		l++;
-	}
-	return (l);
-}
 
 /*
 ** This function converts an integer to a numeric string.
@@ -61,7 +31,9 @@ char	*ft_itoa(int n)
 
 	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
-	i = get_len(n);
+	i = ft_nbrlen(ft_abs(n), 10);
+	if (n < 0)
+		i++;
 	s = (char *)malloc(sizeof(char) * (i + 1));
 	if (!s)
 		return (NULL);
