b442b3877f4ee9e1725981eb4cf2b50ffc1bae08
mlanca-c <mlanca-c@student.42lisboa.com>
2021-05-19 13:07:56 +0100

    Update ft_atoi.c Helper functions moved to their own files. No testing done ...:

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 18:07:37 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/05/12 22:56:08 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/05/19 10:15:51 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 /*
 ** This function converts a ASCII string to an integer.
-** The atoi() function converts the initial portion of the string pointed to
+** The ft_atoi() function converts the initial portion of the string pointed to
 ** by str to int representation.
 **
 ** @param	const char	*str	- string to be converted to int.
@@ -25,28 +25,26 @@
 int	ft_atoi(const char *str)
 {
 	int				i;
-	int				neg;
-	long long int	num;
+	int				negative;
+	long long int	number;
 
 	i = 0;
-	while (str[i] == ' ' || str[i] == '\f'
-		|| str[i] == '\n' || str[i] == '\r'
-		|| str[i] == '\t' || str[i] == '\v')
+	while (ft_isspace(str[i]))
 		i++;
-	neg = 1;
+	negative = 1;
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i++] == '-')
-			neg = -1;
+			negative = -1;
 	}
-	num = 0;
-	while (str[i] >= '0' && str[i] <= '9')
+	number = 0;
+	while (ft_isdigit(str[i]))
 	{
-		num = (num * 10) + (neg * (str[i++] - '0'));
-		if (num > 2147483648)
+		number = (number * 10) + (negative * (str[i++] - '0'));
+		if (number > 2147483648)
 			return (-1);
-		if (num < -2147483648)
+		if (number < -2147483648)
 			return (0);
 	}
-	return (num);
+	return (number);
 }
