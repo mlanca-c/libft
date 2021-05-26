8e272d38f2ca975ac81ddbd2f845366ecc79f3e7
mlanca-c <mlanca-c@student.42lisboa.com>
2021-05-26 15:23:29 +0100

    Create ft_stack_duplicate.c+

--- stack/ft_stack.h
+++ stack/ft_stack.h
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 15:16:19 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/05/26 15:16:22 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/05/26 15:19:08 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,16 +33,12 @@ int		ft_stack_is_duplicate(t_stack *stack);
 int		ft_stack_is_sorted(t_stack *stack);
 int		ft_stack_max_value(t_stack *stack_a);
 int		ft_stack_min_value(t_stack *stack_a);
-
+t_stack	*ft_stack_duplicate(t_stack *stack);
 /*
 ** ft_stack_sort.c Functions
 */
 void	ft_stack_sort(t_stack **stack);
-<<<<<<< HEAD
-t_stack	*ft_stack_duplicate(t_stack *stack);
-=======
 t_stack	*ft_merge_sort(t_stack *stack_a, t_stack *stack_b);
 void	ft_stack_split(t_stack *head, t_stack **stack_a, t_stack **stack_b);
->>>>>>> abd1c21bba14fc24d096735e65ecfe597bc3cdc7
 
 #endif
