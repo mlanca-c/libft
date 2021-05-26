aac126236550f1a6ca3cdb2e46db0ed49ec6506a
mlanca-c <mlanca-c@student.42lisboa.com>
2021-05-26 15:13:46 +0100

    Create ft_stack_duplicate.c+

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/05/26 00:25:45 by mlanca-c          #+#    #+#              #
-#    Updated: 2021/05/26 00:25:53 by mlanca-c         ###   ########.fr        #
+#    Updated: 2021/05/26 15:13:10 by mlanca-c         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -48,7 +48,8 @@ STACK		=	stack/ft_stack_new.c stack/ft_stack_add_front.c \
 				stack/ft_stack_remove.c stack/ft_stack_print.c \
 				stack/ft_stack_clear.c stack/ft_stack_is_sorted.c \
 				stack/ft_stack_max_value.c stack/ft_stack_min_value.c \
-				stack/ft_stack_is_duplicate.c stack/ft_stack_sort.c
+				stack/ft_stack_is_duplicate.c stack/ft_stack_sort.c \
+				stack/ft_stack_duplicate.c
 STACK_O		=	$(STACK:.c=.o)
 
 	# get_next_line Variables #
---
+++ stack/ft_stack_duplicate.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stack_duplicate.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/26 15:08:03 by mlanca-c          #+#    #+#             */
+/*   Updated: 2021/05/26 15:12:22 by mlanca-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_stack.h"
+
+/*
+** This function duplicates a stack.
+** The ft_stack_duplicate() function copies a stack 'stack' and creates a
+** duplicate of that stack 'duplicate'.
+*/
+t_stack	*ft_stack_duplicate(t_stack *stack)
+{
+	t_stack	*duplicate;
+	t_stack	*new_node;
+
+	duplicate = 0;
+	while (stack)
+	{
+		new_node = ft_stack_new(stack->data);
+		if (!new_node)
+		{
+			ft_stack_clear(duplicate);
+			return (NULL);
+		}
+		ft_stack_add_back(duplicate, new_node);
+		stack = stack->next;
+	}
+	return (duplicate);
+}
