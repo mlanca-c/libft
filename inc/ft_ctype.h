8759b73d4d5a3cdf7153e10089fca859e65bf027
mlanca-c <marialsottomayor@gmail.com>
2022-04-23 21:25:29 +0100

    Created ctype library

---
+++ inc/ft_ctype.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ctype.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/23 18:59:56 by mlanca-c          #+#    #+#             */
+/*   Updated: 2022/04/23 20:13:13 by mlanca-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_CTYPE_H
+# define FT_CTYPE_H
+
+/* C Library - <ctype.h> */
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_iscntrl(int c);
+int	ft_isdigit(int c);
+int	ft_isgraph(int c);
+int	ft_islower(int c);
+int	ft_isprint(int c);
+int	ft_ispunct(int c);
+int	ft_isspace(int c);
+int	ft_isupper(int c);
+int	ft_isxdigit(int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+
+#endif /* FT_CTYPE_H */
